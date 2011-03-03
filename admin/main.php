d693fe23ed971ae90c77e3d7fdfd28a7b79ed155
dlinhvan <dlinhvan@79f128f8-fced-9832-224d-cd0538482775>
2011-03-03 08:30:32 +0000

    Tạo cở sở ban đầu

---
+++ admin/main.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @Project NUKEVIET CMS 3.0
+ * @Author VINADES (contact@vinades.vn)
+ * @Copyright (C) 2010 VINADES. All rights reserved
+ * @Createdate 2-9-2010 14:43
+ */
+
+if ( ! defined( 'NV_IS_BOOKTOUR_ADMIN' ) ) die( 'Stop!!!' );
+
+$page_title = $module_info['test content'];
+$contents = "";
+$contents.= "test noi dugn";
+include ( NV_ROOTDIR . "/includes/header.php" );
+echo nv_admin_theme( $contents );
+include ( NV_ROOTDIR . "/includes/footer.php" );
+
+?>
